e74a288b800337827d723ea2dd403de3b53a2cc8
Adam Thomason <adamthomason@Adams-MacBook-Pro.local>
2018-11-23 12:01:26 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::get('/', 'WelcomeController@show');
+
+Route::get('/home', 'HomeController@show');
